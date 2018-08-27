a10947aa3f29e4601709009b0bd1163407acdc7e
WhoisJohnKim <johnkim9417@gmail.com>
2018-08-27 11:39:04 -0500

    Rendering HTML using res.end and internal package fs to read HTML files

--- servers.js
+++ servers.js
@@ -1,47 +1,35 @@
 // Require/import the HTTP module
 var http = require("http");
-
+var fs = require('fs');
 var PORT = 7000;
-// var PORT2 = 7500;
 
-var nice = ["You look nice today",
-            "I really like your smile",
-            "That is a good color on you!",
-            "Have you lost some weight?"];
-var mean = ["Probably should have thought twice about that outfit",
-            "Maybe less fast food yeah?",
-            "Just curious but are you color blind?",
-            "I think it's good your parents still support you despite everything"];
+var server = http.createServer(handleRequest);
 
-function port1HandleRequest(request, response){
-    var random = Math.floor(Math.random() * nice.length);
-    var path = request.url;
+function handleRequest(req, res){
+    var path = req.url;
     switch(path){
         case "/":
-            response.end("Hello fellow human!")
+            readFile(req, res, "/home.html");
             break;
-        case "/nice":
-            response.end(nice[random]);
+        case "/foods":
+            readFile(req, res, "/foods.html");
             break;
-        case "/mean":
-            response.end(mean[random]);
+        case "/movies":
+            readFile(req, res, "/movies.html");
+            break;
+        case "/frameworks":
+            readFile(req, res, "/frameworks.html");
             break;
     }
-    // response.end(nice[random]);
 }
 
-// function port2HandleRequest(request, response){
-//     var random = Math.floor(Math.random() * mean.length);
-//     response.end(mean[random]);
-// }
+function readFile(req, res, fileName){
+    fs.readFile(__dirname + fileName, function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+}
 
-var server1 = http.createServer(port1HandleRequest);
-// var server2 = http.createServer(port2HandleRequest);
-
-server1.listen(PORT, function(){
-    console.log("Server is listening on: http://localhost:" + PORT);
-})
-
-// server2.listen(PORT2, function(){
-//     console.log("Mean server is listening on: http://localhost:" + PORT2);
-// })
+server.listen(PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+  });
